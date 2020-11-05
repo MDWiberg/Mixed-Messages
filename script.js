63db7d293820ddef1f9a8ab1339c2e5e09029694
Matthew Wiberg <darianwiberg@yahoo.com>
2020-11-05 13:04:39 -0500

    Add messages object containing three arrays with three options for a random message

---
+++ script.js
@@ -0,0 +1,6 @@
+const Messages = {
+  firstPart: ['It may', 'It may not', 'It certainly will'],
+  secondPart: [' be',' happen',' not happen'],
+  thirdPart: [' soon.',' way in the future.',' any second.']
+};
+
