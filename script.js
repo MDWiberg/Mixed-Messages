156d7cc748f0363b59ee0c7179f28b94b041e3db
Matthew Wiberg <darianwiberg@yahoo.com>
2020-11-05 13:15:37 -0500

    Add function to generate random messages

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ const Messages = {
   thirdPart: [' soon.',' way in the future.',' any second.']
 };
 
+const generateRandomMessage = () => {
+  let randNumOne = Math.floor(Math.random()*3);
+  let randNumTwo = Math.floor(Math.random()*3);
+  let randNumThree = Math.floor(Math.random()*3);
+  console.log(Messages.firstPart[randNumOne] + Messages.secondPart[randNumTwo] + Messages.thirdPart[randNumThree]);
+};
+
+generateRandomMessage();
