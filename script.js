f6c6026bd4744cebd1a2f06d4cdeb9e570c2751a
Matthew Wiberg <darianwiberg@yahoo.com>
2020-11-05 15:20:27 -0500

    Add comments to explain each section of file and change daredevil nickname personality

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
+// Object to hold random parts necessary for the output message
 const messages = {
   nickName: ['Daredevil', 'Scaredy-Cat', 'Couch-Potato', 'The-Impaler'],
   action: ['Explore your new personality!','Maybe you should try and hide this new personality.','Don\'t be afraid of your new personality.'],
   timeline: ['ten years','rest of your life','a couple of days'],
 };
 
+// Object to hold personalities related to the specific nickname
 const personality = {
-  Daredevil: 'insane and curious',
+  Daredevil: 'down for anything',
   'Scaredy-Cat': 'fearful and timid',
   'Couch-Potato': 'beyond lazy',
   'The-Impaler': 'extremely violent', 
 };
 
+// Generate random number accepting a number parameter, in this case the current array length
 const randomNum = (num) => {
   let randNum = Math.floor(Math.random()*num);
   return randNum;
 };
 
+// Generate message content parts and log the full message output to the screen
 const generateRandomMessage = () => {
 let messageContent = [];
   for(let key in messages) {
